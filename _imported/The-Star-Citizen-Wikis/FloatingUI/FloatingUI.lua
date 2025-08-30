1b7c2b28bbaf52c5875644dccdcde994a1656679
wlft <100052562+wlft@users.noreply.github.com>
2025-08-31 08:55:32 +1000

    imports from tscw

---
+++ _imported/The-Star-Citizen-Wikis/FloatingUI/FloatingUI.lua
@@ -0,0 +1,112 @@
+-- imported from https://github.com/The-Star-Citizen-Wikis/SharedModules
+
+--- Lua functions for Extension:FloatingUI
+---
+--- WARNING
+--- -------
+--- THIS IS AN EXPERIMENTAL MODULE MADE FOR AN EXPERIMENTAL EXTENSION
+--- THIS IS NOT READY FOR PRODUCTION AND SUBJECT TO CHANGE
+--- -------
+local FloatingUI = {}
+
+
+--- Check if a string is empty
+---
+--- @param str string
+--- @return boolean
+local function isStringEmpty( str )
+    return str == nil or str == ''
+end
+
+
+--- Return the HTML of the FloatingUI section component as string
+---
+--- @param data table {label, data, desc, col, inline)
+--- @return string html
+function FloatingUI.renderSection( data )
+    if data == nil or type( data ) ~= 'table' or next( data ) == nil then return '' end
+
+    local htmlTag = 'div'
+    if data['inline'] == true then
+        htmlTag = 'span'
+    end
+
+    local html = mw.html.create( htmlTag )
+        :addClass( 't-floatingui-section' )
+
+    if data['col'] then html:addClass( 't-floatingui-section--cols-' .. data['col'] ) end
+
+    local dataOrder = { 'label', 'data', 'desc' }
+    for _, key in ipairs( dataOrder ) do
+        if data[key] then
+            html:tag( htmlTag )
+                :addClass( 't-floatingui-' .. key )
+                :wikitext( data[key] )
+        end
+    end
+    return tostring( html )
+end
+
+--- Load FloatingUI library only
+---
+--- @return string wikitext Wikitext to load the FloatingUI library only
+function FloatingUI.load( frame )
+    frame = frame or mw.getCurrentFrame()
+    return frame:extensionTag {
+        name = 'templatestyles', args = { src = 'Module:FloatingUI/styles.css' }
+    } .. frame:callParserFunction {
+        name = '#floatingui', args = { '' }
+    }
+end
+
+--- Attach the FloatingUI content to the HTML element
+---
+--- @param content string
+--- @param htmlTag string
+--- @return table|nil
+function FloatingUI.getContentHtml( content, htmlTag )
+    if isStringEmpty( content ) then
+        return
+    end
+
+    -- TODO: Detect if HTML is a div or a span
+    htmlTag = htmlTag or 'div'
+
+    local html = mw.html.create( 'div' ):addClass( 'ext-floatingui-content' )
+        :tag( htmlTag )
+        :addClass( 'mw-parser-output' )
+        :tag( htmlTag )
+        :addClass( 't-floatingui' )
+        :wikitext( content )
+        :allDone()
+
+    return html
+end
+
+--- Render the HTML for FloatingUI
+---
+--- @param reference string Reference wikitext to trigger the floating element
+--- @param content string Content wikitext in the floating element
+--- @param inline boolean Whether to render inline
+--- @return string wikitext Wikitext for the HTML required to use FloatingUI
+function FloatingUI.render( reference, content, inline )
+    if not reference or not content then
+        return ''
+    end
+
+    local htmlTag = 'div'
+    if inline == true then
+        htmlTag = 'span'
+    end
+
+    local html = mw.html.create()
+        :tag( htmlTag )
+        :addClass( 'ext-floatingui-reference' )
+        :wikitext( reference )
+        :done()
+        :node( FloatingUI.getContentHtml( content, htmlTag ) )
+
+    return tostring( html )
+end
+
+return FloatingUI
---
+++ _imported/The-Star-Citizen-Wikis/i18n/i18n.lua
@@ -0,0 +1,164 @@
+require( 'strict' )
+
+-- imported from https://github.com/The-Star-Citizen-Wikis/SharedModules
+
+local i18n = {}
+
+local metatable = {}
+local methodtable = {}
+
+metatable.__index = methodtable
+
+local libraryUtil = require( 'libraryUtil' )
+local checkType = libraryUtil.checkType
+
+--- Cache table containing i18n data
+--- e.g. cache['en']['SMW'] will get you the SMW table in English
+local cache = {}
+
+--- Cache language codes for reuse
+local languages = {}
+
+
+--- Retrieve dataset namespace from key prefix
+---
+--- @param key string The translation key
+--- @return string
+local function getNamespace( key )
+    local namespace = string.match( key, '([^_]*)' )
+    return namespace
+end
+
+
+--- Retrieve a list of applicable language codes
+---
+--- @return table
+local function getLanguageCodes()
+    if #languages > 0 then return languages end
+    local mwlang = mw.language.getContentLanguage()
+    local langCodes = { mwlang:getCode() }
+
+    local fallbackLangCodes = mwlang:getFallbackLanguages()
+    if next( fallbackLangCodes ) ~= nil then
+        for _, fallbackLangCode in pairs( fallbackLangCodes ) do
+            table.insert( langCodes, fallbackLangCode )
+        end
+    end
+
+    --mw.log( string.format( '🌐 [i18n] Setting language chain: %s', table.concat( langCodes, '→' ) ) )
+    return langCodes
+end
+
+
+--- Loads a dataset and saves it to the cache
+---
+--- @param lang string
+--- @param namespace string
+--- @return table|nil { data = "The dataset", keys = "Translation key mapped to index" }
+local function load( lang, namespace )
+    -- Init language cache if it does not exist
+    if cache[ lang ] == nil then
+        cache[ lang ] = {}
+    end
+
+    if cache[ lang ][ namespace ] then
+        return cache[ lang ][ namespace ]
+    end
+
+    local datasetName = string.format( 'Module:i18n/%s/%s.json', namespace, lang )
+    local success, data = pcall( mw.loadJsonData, datasetName )
+
+    if not success then
+        --mw.log( string.format( '🚨 [i18n] Loading dataset[%s][%s]: %s not found on wiki', lang, namespace, datasetName ) )
+        -- Cache the empty result so we do not run mw.loadJsonData again
+        cache[ lang ][ namespace ] = {}
+        return
+    end
+
+    cache[ lang ][ namespace ] = data
+    --mw.log( string.format( '⌛ [i18n] Loading dataset[%s][%s]: %s', lang, namespace, datasetName ) )
+
+    return cache[ lang ][ namespace ]
+end
+
+
+--- Returns translated message (or key if returnKey is enabled)
+---
+--- @param key string The translation key
+--- @param options table|nil Optional options
+--- @return string|nil
+function methodtable.translate( self, key, options )
+    options = options or {
+        ['returnKey'] = true
+    }
+
+    checkType( 'Module:i18n.translate', 1, self, 'table' )
+    checkType( 'Module:i18n.translate', 2, key, 'string' )
+    checkType( 'Module:i18n.translate', 3, options, 'table' )
+
+    --mw.log( string.format( '🔍 [i18n] Looking for message: %s', key ) )
+
+    local namespace = getNamespace( key )
+    if namespace == nil then
+        -- No namespace found error
+        --mw.log( string.format( '❌ [i18n] Namespace cannot be found from: %s', key ) )
+        if options['returnKey'] == true then
+            return key
+        else
+            return
+        end
+    end
+
+    languages = getLanguageCodes()
+
+    local message
+    local i = 1
+
+    while ( message == nil and i <= #languages ) do
+        local dataset = load( languages[ i ], namespace )
+        if dataset then
+            local match = dataset[ key ]
+            if match then
+                message = match
+                --mw.log( string.format( '✅ [i18n] Found message: %s', message ) )
+            end
+        end
+        i = i + 1
+    end
+
+    if message == nil then
+        --mw.log( string.format( '❌ [i18n] Could not found message: %s', key ) )
+        if options['returnKey'] == true then
+            message = key
+        end
+    end
+
+    return message
+end
+
+
+--- Normalize a string to be used as an i18n key by converting whitespace to underscores and making all characters lowercase
+---
+--- @param str string The string to normalize
+--- @return string
+function methodtable.normalizeKey( self, str )
+    checkType( 'Module:i18n.normalizeKey', 1, self, 'table' )
+    checkType( 'Module:i18n.normalizeKey', 2, str, 'string' )
+
+    return string.lower( string.gsub( str, '%s+', '_' ) )
+end
+
+
+--- New Instance
+---
+--- @return table i18n
+function i18n.new( self )
+    local instance = {}
+
+    setmetatable( instance, metatable )
+
+    return instance
+end
+
+
+return i18n
