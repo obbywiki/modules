6c71567e38bbe527481b69e761127306bf74a05a
Wolfite <100052562+wlft@users.noreply.github.com>
2025-08-31 16:21:06 +1000

    more

--- ObbyGameInfobox/ObbyGameInfobox.lua
+++ ObbyGameInfobox/ObbyGameInfobox.lua
@@ -2,6 +2,36 @@
 
 local ObbyGameInfobox = {}
 
+local function month_by_index(month)
+	if month == 1 then
+		return 'January'
+	elseif month == 2 then
+		return 'Feburary'
+	elseif month == 3 then
+		return 'March'
+	elseif month == 4 then
+		return 'April'
+	elseif month == 5 then
+		return 'May'
+	elseif month == 6 then
+		return 'June'
+	elseif month == 7 then
+		return 'July'
+	elseif month == 8 then
+		return 'August'
+	elseif month == 9 then
+		return 'September'
+	elseif month == 10 then
+		return 'October'
+	elseif month == 11 then
+		return 'November'
+	elseif month == 12 then
+		return 'December'
+	else
+		return 'N/A'
+	end
+end
+
 function ObbyGameInfobox.main( frame )
     local InfoboxNeue = require( 'Module:InfoboxNeue' )
 
@@ -13,10 +43,34 @@ function ObbyGameInfobox.main( frame )
 
     local obby_name = args.name or '{{PAGENAME}}'
     local obby_starter_place_id = args.root_place_id or args.start_place_id or 1818
+	local obby_subgenre = args.subgenre or args.sub_genre or 'N/A'
+	local obby_maturity = args.maturity or args.rating or 'na'
+
+	obby_maturity = string.lower(obby_maturity)
+
+	if obby_maturity == 'minimal' then
+		obby_maturity = 'Minimal - Ages 5+'
+	elseif obby_maturity == 'mild' then
+		obby_maturity = 'Mild - Ages 9+'
+	elseif obby_maturity == 'mature' then
+		obby_maturity = 'Mature - Ages 13+'
+	elseif obby_maturity == 'restricted' then
+		obby_maturity = 'Restricted - Ages 18+'
+	elseif obby_maturity == 'unrated' or obby_maturity == 'na' then
+		obby_maturity = 'Unrated - Ages 18+'
+	else
+		obby_maturity = 'N/A - Unknown'
+	end
+
 
 	local obby_developer = args.developer or args.creator or 'Unknown'
-    local obby_publisher = args.publisher or 'Self-published'
+    local obby_publisher = args.publisher or 'Self-Published'
     local obby_creation_year = args.year or ''
+	local obby_creation_month = month_by_index(tonumber(args.month or '0') or 0)
+
+	local obby_stats_visits = args.visits or 'N/A'
+	local obby_stats_peak_ccu = args.peak_ccu or 'N/A'
+	local obby_stats_likes = args.likes or 'N/A'
 
     local obby_levels = args.levels or args.stages or 'N/A'
 
@@ -53,12 +107,25 @@ function ObbyGameInfobox.main( frame )
 		}
 	} )
 
-    test:renderSection( {
-		title = 'Physical Description',
+	test:renderSection( {
+		title = 'Statistics',
 		col = 2,
 		content = {
-			test:renderItem( 'Height', 'idk' ),
-			test:renderItem( 'Mass', 'idk' )
+			test:renderItem( 'Visits', obby_stats_visits),
+			test:renderItem( 'Peak CCU', obby_stats_peak_ccu ),
+			test:renderItem( 'Likes', obby_stats_likes ),
+		}
+	} )
+
+    test:renderSection( {
+		title = 'Publishing',
+		col = 2,
+		content = {
+			test:renderItem( 'Released', obby_creation_month .. ' ' .. obby_creation_year ),
+			test:renderItem( 'Publisher', obby_publisher ),
+			test:renderItem( 'Genre', 'Obby & Platformer' ),
+			test:renderItem( 'Sub-genre', obby_subgenre ),
+			test:renderItem( 'Maturity', '' ),
 		}
 	} )
 
