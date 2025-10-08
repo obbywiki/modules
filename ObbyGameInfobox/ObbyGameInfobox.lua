34f409eb1c7299ba54cd67fc72d9cf4d8267fe31
wlft <100052562+wlft@users.noreply.github.com>
2025-10-08 19:39:29 +1000

    correct subgenre snake_case entries

--- ObbyGameInfobox/ObbyGameInfobox.lua
+++ ObbyGameInfobox/ObbyGameInfobox.lua
@@ -108,11 +108,19 @@ function ObbyGameInfobox.main( frame )
 
 	if obby_subgenre_lower == 'dco' then
 		obby_subgenre = 'Difficulty Chart Obby'
+	elseif obby_subgenre_lower == 'difficulty_chart_obby' then
+		obby_subgenre = 'Difficulty Chart Obby'
 	elseif obby_subgenre_lower == 'st' then
+		obby_subgenre = 'Stage Tower Obby'
+	elseif obby_subgenre_lower == 'stage_tower_obby' then
 		obby_subgenre = 'Stage Tower Obby'
 	elseif obby_subgenre_lower == 't' then
 		obby_subgenre = 'Tower Obby'
+	elseif obby_subgenre_lower == 'tower_obby' then
+		obby_subgenre = 'Tower Obby'
 	elseif obby_subgenre_lower == 'so' then
+		obby_subgenre = 'Story Obby'
+	elseif obby_subgenre_lower == 'story_obby' then
 		obby_subgenre = 'Story Obby'
 	end
 
