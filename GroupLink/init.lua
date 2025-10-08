ffba1284bb9a53b0bf30da69874e8c3881e9a237
Wolfite <100052562+wlft@users.noreply.github.com>
2025-10-08 18:14:19 +1000

    GroupLink/init.lua

---
+++ GroupLink/init.lua
@@ -0,0 +1,41 @@
+local p = {}
+local vc = require('Module:VerifiedConfig')
+
+local function page_exists(page_title)
+    local title = mw.title.new(page_title)
+    return title and title.exists
+end
+
+local function is_verified_group(page_title)
+    for _, groupName in ipairs(vc.verified_groups) do
+        if groupName == page_title then
+            return true
+        end
+    end
+    return false
+end
+
+function p.link(frame)
+    local args = frame:getParent().args
+    local page_name = args[1] or ''
+
+    if page_name == '' then
+        return ''
+    end
+
+    local output = ''
+    local checkmark = '[[File:Roblox_Verification_Badge.svg|15px|link=|alt=Verified]]'
+
+    if page_exists(page_name) then
+        output = '[[' .. page_name .. '|' .. page_name .. ']]'
+        if is_verified_group(display_text) then
+            output = output .. ' ' .. checkmark
+        end
+    else
+        output = '[https://roblox.com/communities/0/' .. page_name .. ' ' .. page_name .. ' ' .. checkmark .. ']'
+    end
+
+    return output
+end
+
+return p
