9ec5d23fdf17031de7860c94f03165674b573f23
wlft <100052562+wlft@users.noreply.github.com>
2025-10-08 23:02:22 +1000

    minor formatting fix

--- ObbyGameInfobox/ObbyGameInfobox.lua
+++ ObbyGameInfobox/ObbyGameInfobox.lua
@@ -228,7 +228,7 @@ function ObbyGameInfobox.main( frame )
 				obby_developer = obby_developer .. ' [[File:Roblox_Verification_Badge.svg|12px|alt=Verified]]'
 			else
 				obby_developer = string.format(
-					'[https://roblox.com/%s/%s/ %s %s]',
+					'[https://roblox.com/%s/%s/loading %s %s ]',
 					base, c.id, c.name,
 					(c.hasVerifiedBadge and ' [[File:Roblox_Verification_Badge.svg|12px|alt=Verified]]') or ''
 				)
