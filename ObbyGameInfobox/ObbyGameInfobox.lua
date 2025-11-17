377d025ffd4be68393c70a203514e1ae2486d9d9
wlft <100052562+wlft@users.noreply.github.com>
2025-11-17 18:45:52 +1000

    add more verified statuses

--- ObbyGameInfobox/ObbyGameInfobox.lua
+++ ObbyGameInfobox/ObbyGameInfobox.lua
@@ -91,7 +91,8 @@ function ObbyGameInfobox.main( frame )
 
 	local obby_subgenre_lower = string.lower(obby_subgenre)
 
-	local obby_is_verified = (args.verified == 'true' or args.verified == 'full') and true or false
+	-- local obby_is_verified = (args.verified == 'true' or args.verified == 'full') and true or false
+	local obby_verified_status = (args.verified == 'true' or args.verified == 'full') and 'verified' or args.verified == 'false' and 'unstable' or 'unknown'
 
 	if obby_subgenre_lower == 'dco' then
 		obby_subgenre = 'Difficulty Chart Obby'
@@ -385,7 +386,7 @@ function ObbyGameInfobox.main( frame )
 		)
 	end
 
-	if obby_is_verified then
+	-- if obby_verified_status then
 		test:renderSection({
 			title = 'Playability',
 			col = 2,
@@ -394,19 +395,19 @@ function ObbyGameInfobox.main( frame )
 					{
 						label = 'Status',
 						plainlinks_enabled = true,
-						data = '[[File:Verified-check-green-96.webp|36px|alt=Verified|link=]]'
+						data = obby_verified_status == 'verified' and '[[File:Verified-check-green-96.webp|36px|alt=Verified|link=]]' or obby_verified_status == 'unstable' and '[[File:Verified-dash-red-72.webp|36px|alt=Unstable|link=]]' or obby_verified_status == 'unknown' and '[[File:Verified-dash-orange-72.webp|36px|alt=Unknown|link=]]'
 					}
 				),
 				test:renderItem(
 					{
 						label = 'Info',
 
-						data = 'Verified - Fully Possible - All Devices'
+						data = obby_verified_status == 'verified' and 'Verified - Fully Possible - All Devices' or obby_verified_status == 'unstable' and 'Unstable - Partially Possible - Some Devices' or 'Unknown'
 					}
 				)
 			}
 		})
-	end
+	-- end
 
 	test:renderSection( {
 		title = 'Technical',
