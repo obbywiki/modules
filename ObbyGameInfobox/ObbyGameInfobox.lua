9dc87a612d1a4b2099168ba48b7597171dd708d3
Wolfite <100052562+wlft@users.noreply.github.com>
2025-11-18 11:32:07 +1000

    patch(ObbyGameInfobox.lua): attempt to use piped SHORTDESC template

--- ObbyGameInfobox/ObbyGameInfobox.lua
+++ ObbyGameInfobox/ObbyGameInfobox.lua
@@ -494,7 +494,7 @@ function ObbyGameInfobox.main( frame )
 		table.insert(append_categories, '[[Category:' .. parsed_month .. ']]')
 	end
 
-	local shortdesc = '{{SHORTDESC:' .. (obby_subgenre .. ' by ' .. obby_developer_canonical .. ' - ' .. obby_creation_year) .. '}}'
+	local shortdesc = '{{SHORTDESC|' .. (obby_subgenre .. ' by ' .. obby_developer_canonical .. ' - ' .. obby_creation_year) .. '}}'
 
     return rendered .. '\n' .. table.concat(append_categories, '\n').. '\n' .. shortdesc
 end
