2ae0caf41643db7138f7c0c3a7fe94ae6df72365
wlft <100052562+wlft@users.noreply.github.com>
2026-02-26 04:39:11 +1000

    annoying

--- ObbyGameInfobox/ObbyGameInfobox.lua
+++ ObbyGameInfobox/ObbyGameInfobox.lua
@@ -4,13 +4,17 @@ local ObbyGameInfobox = {}
 
 local i18n = require('Module:i18n2').new('ObbyGameInfobox')
 
+local months_full = {'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'}
+
 local function month_by_index(month)
-	local months = i18n:get('months', {})
+	-- local months = i18n:get('months', {})
 	local monthnum = tonumber(month)
 
 	if not monthnum then return i18n:get('label_na', 'N/A') end
 
-	return months[tostring(monthnum)] or i18n:get('label_na', 'N/A')
+	return months_full[monthnum] or i18n:get('label_na', 'N/A')
+
+	-- return months[tostring(monthnum)] or i18n:get('label_na', 'N/A')
 end
 
 local function get_comma_val(num)
