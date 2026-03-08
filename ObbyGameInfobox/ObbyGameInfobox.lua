ee93ae5ffd93cf7eb8c0fcf25663bfb77e59e18e
wlft <100052562+wlft@users.noreply.github.com>
2026-03-08 15:17:16 +1000

    feat(ObbyGameInfobox): add publisher to cargo data

--- ObbyGameInfobox/ObbyGameInfobox.lua
+++ ObbyGameInfobox/ObbyGameInfobox.lua
@@ -90,6 +90,7 @@ local obby_schema = {
 	thumbnail = 'String',
 	creator = 'String', -- aka. "Developer"
 	developers = 'List (,) of String',
+	publisher = 'String',
 	
 	stages = 'Integer',
 	tier = 'Integer',
@@ -779,6 +780,7 @@ function ObbyGameInfobox.main( frame )
 			universe_id = tostring(universe_id),
 			name = args.name or mw.title.getCurrentTitle().text,
 			thumbnail = thumb,
+			publisher = obby_publisher,
 			creator = obby_developer_canonical or obby_developer_raw,
 			stages = args.stages,
 			tier = args.tier,
