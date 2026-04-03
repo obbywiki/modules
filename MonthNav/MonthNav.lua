3cb4d7eeac0ac5aba176aadf530e45c2884d5dfa
wlft <100052562+wlft@users.noreply.github.com>
2026-04-04 01:27:24 +1000

    feat: MonthNav

---
+++ MonthNav/MonthNav.lua
@@ -0,0 +1,52 @@
+local p = {}
+
+local html = mw.html
+
+local months_full = {'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'}
+
+function p.draw(frame)
+    local args = frame:getParent().args
+    local target_year = args[1] or os.date("%Y")
+    local current_month_name = args[2] or ""
+    
+    local container = html.create('div')
+        :addClass('ow-month-nav')
+
+    for i, month in ipairs(months_full) do
+        local count = 0
+
+        if mw.ext.cargo and mw.ext.cargo.query then
+            local data = mw.ext.cargo.query('Obbies', 'COUNT(*)=count', {
+                where = "month='" .. tostring(i) .. "' AND year='" .. target_year .. "'"
+            })
+            if data and data[1] then
+                count = tonumber(data[1].count) or 0
+            end
+        end
+
+        local isActive = (month:lower() == current_month_name:lower())
+        local isEmpty = (count == 0)
+
+        local card = container:tag('div')
+
+        card:addClass('ow-month-card')
+        if isActive then card:addClass('ow-month-card is-active') end
+        if isEmpty then card:addClass('ow-month-card is-empty') end
+
+        card:tag('div')
+            :addClass('ow-month-card__label')
+            :wikitext(month .. ' ' .. target_year)
+
+        card:tag('div')
+            :addClass('ow-month-card__count')
+            :wikitext(count .. ' obbies')
+
+        card:tag('span')
+            :addClass('ow-month-card__link')
+            :wikitext('[https://obbywiki.com/wiki/Category:' .. month .. '_' .. target_year .. ' ]')
+    end
+
+    return frame:extensionTag('templatestyles', '', { src = 'Template:MonthNav/styles.css' }) .. tostring(container)
+end
+
+return p
