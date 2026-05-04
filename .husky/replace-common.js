434b4ff9a5b442771fd08f26ebdc08ee24f8b0f4
Wolfite <100052562+wlft@users.noreply.github.com>
2026-05-05 01:18:17 +1000

    setup husky

---
+++ .husky/replace-common.js
@@ -0,0 +1,12 @@
+import fs from 'fs';
+
+const msg_path = process.argv[2];
+
+if (msg_path) {
+  const content = fs.readFileSync(msg_path, 'utf-8');
+  
+  var newc = content
+  newc = content.replace(/fix-ogi:/g, 'fix(ObbyGameInfobox):');
+
+  fs.writeFileSync(msg_path, newc);
+}
