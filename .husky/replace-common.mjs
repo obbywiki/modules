687fa93f0ebf625892e15041a6508b27d68b48d1
Wolfite <100052562+wlft@users.noreply.github.com>
2026-05-05 01:35:14 +1000

    improve hook

--- .husky/replace-common.mjs
+++ .husky/replace-common.mjs
@@ -2,8 +2,6 @@ import fs from 'fs';
 
 const msg_path = process.argv[2];
 
-console.log(msg_path);
-
 if (msg_path) {
   const content = fs.readFileSync(msg_path, 'utf-8');
   
@@ -11,6 +9,11 @@ if (msg_path) {
     .replace(/fix-ogi:/gi, 'fix(ObbyGameInfobox):')
     .replace(/fix-gi:/gi, 'fix(GroupInfobox):')
     .replace(/feat-ogi:/gi, 'feat(ObbyGameInfobox):')
-    .replace(/feat-gi:/gi, 'feat(GroupInfobox):');
+    .replace(/feat-gi:/gi, 'feat(GroupInfobox):')
+    .replace(/chore-ogi:/gi, 'chore(ObbyGameInfobox):')
+    .replace(/chore-gi:/gi, 'chore(GroupInfobox):')
+    .replace(/style-ogi:/gi, 'style(ObbyGameInfobox):')
+    .replace(/style-gi:/gi, 'style(GroupInfobox):');
 
   fs.writeFileSync(msg_path, newc);
+}
