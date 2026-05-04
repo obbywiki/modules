ce12e522ba4d949800aa1a5d0d1eae3413298c9e
Wolfite <100052562+wlft@users.noreply.github.com>
2026-05-05 01:34:22 +1000

    fix: my stupidity

--- .husky/replace-common.mjs
+++ .husky/replace-common.mjs
@@ -2,12 +2,15 @@ import fs from 'fs';
 
 const msg_path = process.argv[2];
 
+console.log(msg_path);
+
 if (msg_path) {
   const content = fs.readFileSync(msg_path, 'utf-8');
   
   var newc = content
-  newc = content.replace(/fix-ogi:/g, 'fix(ObbyGameInfobox):');
-  newc = content.replace(/fix-gi:/g, 'fix(GroupInfobox):');
+    .replace(/fix-ogi:/gi, 'fix(ObbyGameInfobox):')
+    .replace(/fix-gi:/gi, 'fix(GroupInfobox):')
+    .replace(/feat-ogi:/gi, 'feat(ObbyGameInfobox):')
+    .replace(/feat-gi:/gi, 'feat(GroupInfobox):');
 
   fs.writeFileSync(msg_path, newc);
-}
